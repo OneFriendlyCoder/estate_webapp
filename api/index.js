af1133ce47fd98f34e6322d24261fe6bbeb1f39e
OneFriendlyCoder <abhyanand311099@gmail.com>
2023-10-18 18:54:19 +0530

    fixed signup

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose, { mongo } from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -20,7 +21,7 @@ app.listen(3000, () => {
 })
 
 app.use("/api/user", userRouter);
-//app.use("/api/user", authRouter);
+app.use("/api/auth", authRouter);
 
 
 //middleware
