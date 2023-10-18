725cfb851429a231ebd7850aaef6a3b213c9bb9e
OneFriendlyCoder <abhyanand311099@gmail.com>
2023-10-18 11:55:25 +0530

    Adding header and configuring routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import SignIn from "./pages/Signin.jsx";
 import SignUp from "./pages/Signup.jsx";
 import Profile from "./pages/Profile.jsx";
 import About from "./pages/About.jsx";
+import Header from "./components/Header.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
