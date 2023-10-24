6f56bbd364d66be48969846d8f8b522914cc478b
OneFriendlyCoder <abhyanand311099@gmail.com>
2023-10-24 12:24:03 +0530

    user show listing completed

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs'
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
     res.json({
@@ -48,15 +49,15 @@ export const deleteUser = async( req, res, next ) => {
   }
 };
 
-export const getUserListings =  async(req, res, next) => {
-  if(req.user.id !== req.params.id){
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
     try {
-      const listings = await Listing.find({userRef: req.params.id});
+      const listings = await Listing.find({ userRef: req.params.id });
       res.status(200).json(listings);
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }else{
-    return next(errorHandler(401, "You can only view your own listings!"));
+  } else {
+    return next(errorHandler(401, 'You can only view your own listings!'));
   }
-}
+};
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -17,7 +17,7 @@ import {
   signoutUserStart,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
-import {Link} from 'react-router-dom';
+import {Link, useLinkClickHandler} from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -27,6 +27,8 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setshowListingsError] = useState(false);
+  const [userListings, setuserListings] = useState([]);
   const dispatch = useDispatch();
 
 
@@ -120,6 +122,21 @@ export default function Profile() {
     }
   }
 
+  const handleShowListings =  async() => {
+    try {
+      setshowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if(data.success === false){
+        setshowListingsError(true);
+        return;
+      }
+      setuserListings(data);
+    } catch (error) {
+      setshowListingsError(true);
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -199,6 +216,39 @@ export default function Profile() {
       <p className='text-green-700 mt-5'>
         {updateSuccess ? 'User is updated successfully!' : ''}
       </p>
+       <button onClick={handleShowListings} className='text-green-700 w-full'>Show Listings</button>
+       <p className='text-red-700 mt-5'>{showListingsError ? 'Error showing listings':''}</p>
+      
+       {userListings &&
+        userListings.length > 0 &&
+        <div className="flex flex-col gap-4">
+          <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className='border rounded-lg p-3 flex justify-between items-center gap-4'
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt='listing cover'
+                  className='h-16 w-16 object-contain'
+                />
+              </Link>
+              <Link
+                className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+  
+              <div className='flex flex-col item-center'>
+                <button className='text-red-700 uppercase'>Delete</button>
+                <button className='text-green-700 uppercase'>Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>}
     </div>
   );
 }
